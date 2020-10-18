b153bb24ea69f92fc99b1cec844dee4b6985dd33
MetricElectric <66031289+MetricElectric@users.noreply.github.com>
2020-10-18 16:51:48 -0700

    yep

---
+++ Stretching Coach App/Assets/Scripts/MyCoroutine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCoroutine : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canPlayerFire)
+            {
+                StartCoroutine(coRoutineTest());
+            }
+        }
+
+    }
+
+    bool canPlayerFire = true;
+    IEnumerator coRoutineTest()
+    {
+        Debug.Log("Fire");
+
+        canPlayerFire = false;
+
+        yield return new WaitForSeconds(3);
+
+        canPlayerFire = true;
+
+    }
+}
